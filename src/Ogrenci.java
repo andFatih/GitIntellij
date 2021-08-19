4d8991619faad17dbc003802867fb4a38c2d9fe1
andFatih <fatihinmaili@yandex.com>
2021-08-19 20:44:47 +0300

    Versiyon 2: Not giriş metodu eklendi

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -1,6 +1,11 @@
 public class Ogrenci {
     public static void main(String[] args) {
 
-        System.out.println("Projenin ilk aşaması tamamlandı");
+
+        System.out.println("Not girisi bölümü çağrıldı");
+    }
+
+    public static void OgrenciNotEkle(){
+        System.out.println("Not girisi");
     }
 }
